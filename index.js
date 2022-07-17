7dfa58f383455c5640d8e3705b00de71ce78084d
santhosh s <100987620+santhosh06062002@users.noreply.github.com>
2022-07-17 18:44:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let num1 = window.prompt("enter a number 1");
+let num2 = window.prompt("enter a number 2");
+num1 = Number(num1);
+num2 = Number(num2);
+console.log("addtion",num1+num2);
+console.log("subtraction",num1-num2);
+console.log("multiplication",num1*num2);
+console.log("division",num1/num2);
